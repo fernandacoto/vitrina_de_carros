70ba871294e76186dcfe55dc8f8c6cbd89afb63b
Fernanda Coto <fermarioro@gmail.com>
2018-05-08 20:18:46 -0600

    Simple search by brand for cars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CarComponent } from './car/car.component';
 import { CarsService } from './cars.service';
+import { FilterPipe }from './filter.pipe';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarComponent
+    CarComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [CarsService],
   bootstrap: [AppComponent, CarComponent]
--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -7,20 +7,8 @@ import { CarsService } from '../cars.service';
   styleUrls: ['./car.component.css']
 })
 export class CarComponent {
-  cars = [
-    [{name: "Carro1", brand: "Marca 1"},{name: "Carro2", brand: "Marca 2"},{name: "Carro3", brand: "Marca 3"}],
-    [{name: "Carro1", brand: "Marca 1"},{name: "Carro2", brand: "Marca 2"},{name: "Carro3", brand: "Marca 3"}],
-    [{name: "Carro1", brand: "Marca 1"},{name: "Carro2", brand: "Marca 2"},{name: "Carro3", brand: "Marca 3"}]
-  ];
   set_of_cars;
   constructor(service: CarsService) {
     this.set_of_cars = service.getCars();
    }
-  getCarsChunked(){
-    let i,j,temparray,chunk = 3;
-    for (i=0,j=this.set_of_cars.length; i<j; i+=chunk) {
-        temparray = this.set_of_cars.slice(i,i+chunk);
-    }
-    return temparray;
-  }
 }
